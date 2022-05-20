2049625251407ce7451785401304f8cb94b37bdb
mxnxa <alsgk0428@naver.com>
2022-05-20 18:21:51 +0900

    js 문제풀이, google 클론 코딩

---
+++ javascript/test/test.js
@@ -0,0 +1,22 @@
+let x = 5;
+let y = 3;
+let str = "";
+
+// for (let i = 0; i < x; i++) {
+//   str += "*";
+// }
+// for (let j = 0; j < y; j++) {
+//   console.log(str);
+// }
+
+// for (let i = 0; i < x; i++) {
+//   str += "*";
+//   for (let j = 0; j < y; j++) {
+//     console.log(str);
+//   }
+// }
+
+for (let i = 0; i < x; i++) {
+  str += "*";
+  console.log("*".repeat(7));
+}
