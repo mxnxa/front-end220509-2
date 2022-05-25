13c0f76dd2dbaaee1887168e7995e30ad06ea41a
mxnxa <alsgk0428@naver.com>
2022-05-25 15:24:39 +0900

    배열 고차함수 복습

--- javascript/07_object/js/array.js
+++ javascript/07_object/js/array.js
@@ -40,7 +40,7 @@ console.log(arr4.slice(0, 1), arr4);
 // 배열 고차 함수 : 함수의 인자 값으로 함수를 받거나, 함수의 return 값으로 함수를 반환하는 함수.
 // addEventListener("click", function() {})
 
-// forEach
+// 1. forEach
 // 배열에 있는 모든 요소를 함수의 인자로 전달하여 실행.
 // 두번째 인자로 인덱스 값을 전달해준다.
 
@@ -54,7 +54,7 @@ arr5.forEach(function (fruit, i) {
   console.log(`${i + 1}번째 ` + fruit);
 });
 
-// map
+// 2. map
 // 배열의 각 데이터를 가공해서 새로운 배열에 담아서 return
 // map 함수의 인자로 들어온 함수의 return 값을 배열에 담는다.
 
@@ -87,8 +87,59 @@ for (let i = 0; i < arr7.length; i++) {
 }
 console.log(evenArr);
 
-// filter : 인자로 전달된 함수의 return 값이 true인 요소들만 배열에 담아서 배열을 return.
+// 3. filter : 인자로 전달된 함수의 return 값이 true인 요소들만 배열에 담아서 배열을 return.
 let evenArr2 = arr7.filter(function (num) {
   return num % 2 === 0;
 });
 console.log(evenArr2);
+
+// 배열
+let arr8 = [1, 2, 3, 4, 5];
+
+// for문과 배열.length를 통해서 배열을 순회하는 방법 => 배열 고차함수 보다 고성능
+for (let i = 0; i < arr8.length; i++) {
+  console.log(arr[i]);
+}
+
+// 배열 고차함수
+
+// 1. Array.forEach() => 배열을 순회하면서 함수를 실행. return 값 x.
+// 인자로 들어온 함수의 매개변수로 각 아이템을 넣어준다.
+// 함수(첫번째 아이템) 실행 => 함수(두번째 아이템) => ... => 함수(마지막 아이템)
+
+arr8.forEach(function (number) {
+  console.log(number * 2);
+});
+
+// 두번째 인자로 각 아이템의 인덱스를 넣어준다. => map과 filter도 마찬가지
+arr8.forEach(function (number, idx) {
+  console.log(idx + 1 + "번째 아이템");
+});
+
+// 2. Array.filter() => 배열을 순회하면서 함수를 실행하며 필터링
+// => filter 안의 함수의 return 값이 true or false에 따라 필터링
+// => filter 안의 함수의 return 값이 true인 차례에만 해당 아이템을 새로운 배열에 넣어서
+// => filter 함수가 완전히 끝나면 이 배열을 return
+
+let filterArr = arr8.filter((num) => {
+  return num >= 3;
+});
+console.log(filterArr);
+
+// 화살표 함수는 코드블록을 생략하면 바로 값을 return
+let filterArr02 = arr8.filter((num) => num >= 3);
+console.log(filterArr02);
+
+// 3. Array.map() => 배열을 순회하면서 함수를 실행하여 값을 변환
+// => 배열의 각 아이템을 map 안의 함수의 return 값으로 바꾼다.
+// 이 바뀐 값들을 모아서 새로운 배열로 반환
+
+// 함수는 return 값이 없으면 => undefined
+
+let mapArr = arr8.map((num) => {
+  return num + "번째 아이템";
+});
+console.log(mapArr);
+
+let mapArr02 = arr8.map((num) => num + "번째 아이템");
+console.log(mapArr);
