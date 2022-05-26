4ce30d853144c05d74dfcd8f4033e59b5498a48c
mxnxa <alsgk0428@naver.com>
2022-05-26 10:56:08 +0900

    생성자 함수(class) 및 ilty 클론 코딩

--- javascript/12_constructor/constructor.js
+++ javascript/12_constructor/constructor.js
@@ -2,6 +2,7 @@
 // 생성자 : 미리 설계도를 만들어 놓고 그 설계도에 맞는 실체(인스턴스)를 만들어낸다.
 // 인스턴스 : 실제로 메모리를 차지함
 // 인스턴스 생성할 때는 항상 new 키워드와 함께 생성자 함수를 호출
+// 참고) 디자인 패턴
 
 // 생성자 함수 : 제품의 설계도
 // new 생성자함수() : 실제 제품
@@ -50,7 +51,7 @@ console.log(user.type);
 console.log(user.name);
 console.log(user02.name);
 
-// class는 prototype의 문법적 설탕(syntax suger)
+// class는 prototype의 문법적 설탕(syntax suger) => 생성자 함수와 마찬가지로 인스턴스를 만드는 설계도
 class Human {
   constructor(name, age) {
     // 값 초기화
@@ -76,3 +77,90 @@ const human = new Human("minha", 24);
 Human.hello();
 console.log(human.name);
 human.greeting();
+
+// 객체
+function Rect(width, height) {
+  this.width = width;
+  this.height = height;
+}
+
+// prototype에 함수를 추가하면 앞으로 생성될 인스턴스가 사용
+// => 생성자 함수가 실행 x
+Rect.prototype.getArea = function () {
+  return this.width * this.height;
+};
+// Rect.getArea();
+
+const rect = new Rect(10, 10);
+console.log(rect.getArea());
+
+// class
+class Rectangle {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
+  // 인스턴스가 사용할 함수 => 일반적인 함수와 달리 function 키워드 사용 x
+  // 이렇게 함수를 작성하면 prototype에 추가.
+  getArea() {
+    return this.width * this.height;
+  }
+
+  // static : 클래스에서 직접 사용하는 메서드(함수)와 프로퍼티(값) => 인스턴스에서 사용 불가
+  static type = "사각형";
+  static getType() {
+    console.log("사각형입니다.");
+  }
+}
+console.log(Rectangle.type);
+
+const rectByClass = new Rectangle(20, 20);
+console.log(rectByClass);
+Rectangle.getType();
+
+// 클래스 상속
+class Animal {
+  constructor(legs, speed) {
+    this.legs = legs;
+    this.speed = speed;
+  }
+
+  move() {
+    console.log(`${this.speed}km/h로 움직입니다.`);
+  }
+}
+
+// Dog 클래스가 Animal 클래스를 상속 받음 => extends 상속받을 클래스명
+class Dog extends Animal {
+  // constructor를 생략하면 상위 클래스의 constructor 실행
+  // 프로퍼티를 추가하고 싶으면 constructor 내부에서 super() 실행
+  // => constructor에 기존 상위 클래스의 인자 + 서브 클래스만의 인자를 받아야한다.
+  // => super(기존 프로퍼티) + this.새로운 프로퍼티 = 서브 클래스만의 인자
+
+  constructor(legs, speed, age) {
+    // super : 상위 클래스의 constructor 실행
+    // super가 먼저 실행되어야함
+    super(legs, speed);
+    this.age = age;
+  }
+
+  bark() {
+    console.log("멍!");
+  }
+  // 오버라이딩 : 기존의 메서드명과 같은 메서드를 작성하면 덮어씌워진다.
+  // => Animal 클래스의 move()를 덮어씌움
+  move() {
+    console.log("어떻게 될까요?");
+  }
+}
+
+// class Human extends Animal {
+//   constructor()
+// }
+
+const dog = new Dog(4, 10);
+const dog2 = new Dog(4, 15, 2);
+console.log(dog);
+console.log(dog2);
+dog.move();
+dog.bark();
