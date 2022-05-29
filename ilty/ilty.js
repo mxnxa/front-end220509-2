746db8c3ca928de09b9df296a66e01ff62889739
mxnxa <alsgk0428@naver.com>
2022-05-29 21:39:13 +0900

    ilty 클론 코딩(side_menu 추가 및 디테일 수정 등)

--- ilty/ilty.js
+++ ilty/ilty.js
@@ -1,3 +1,12 @@
+let popup = document.querySelector(".popup");
+let closeBtn = document.querySelectorAll(".btns");
+
+closeBtn.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    popup.style.display = "none";
+  });
+});
+
 $(".topNav li").on("mouseleave", function () {
   $(this).children("ul").fadeOut();
 });
