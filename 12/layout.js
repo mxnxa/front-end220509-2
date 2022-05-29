e930b61cfc36a0d545dd947095266f0a97ad6569
mxnxa <alsgk0428@naver.com>
2022-05-29 16:25:29 +0900

    로그인 추가(입력 받고 데이터 출력)

--- 12/layout.js
+++ 12/layout.js
@@ -30,6 +30,34 @@ let sliderUl = document.querySelector(".slider .sliderContainer ul");
 
 btns.forEach(function (btn, idx) {
   btn.addEventListener("click", function () {
-    sliderUl.style.transform = `translateX(${-600 * idx}px)`;
+    sliderUl.style.transform = `translateX(${-800 * idx}px)`;
   });
+});
+
+// login
+document.querySelector(".loginBtn").addEventListener("click", function (e) {
+  e.preventDefault();
+
+  let userId = "test";
+  let userPw = "test123";
+  let id = document.querySelector(".id");
+  let pw = document.querySelector(".pw");
+  let user = [];
+
+  if (userId === id.value && userPw === pw.value) {
+    alert("로그인 성공!");
+  } else if (userId !== id.value && userPw !== pw.value) {
+    alert("아이디와 비밀번호가 일치하지 않습니다.");
+  } else if (userId !== id.value) {
+    alert("아이디가 일치하지 않습니다.");
+  } else if (userPw !== pw.value) {
+    alert("비밀번호가 일치하지 않습니다.");
+  }
+
+  user = [id.value, pw.value];
+  console.log(user);
+
+  id.value = "";
+  pw.value = "";
+  id.focus();
 });
