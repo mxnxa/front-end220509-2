f657bac531690b9f7ce2dd89efd81a45e3513232
mxnxa <alsgk0428@naver.com>
2022-06-05 02:13:14 +0900

    넵플러스 클론 코딩 - 비주얼까지

--- neppplus/neppplus.js
+++ neppplus/neppplus.js
@@ -1,6 +1,32 @@
-let heading = document.querySelector(".nav .menu");
-let menu = document.querySelector(".dropdown");
+let slides = document.querySelectorAll(".slides img");
+let prev = document.querySelector(".prev");
+let next = document.querySelector(".next");
+let current = 0;
 
-heading.onmouseover = function () {
-  menu.style.display = "flex";
+slideShow(current);
+
+function slideShow(current) {
+  slides.forEach((slide) => {
+    slide.style.display = "none";
+  });
+  slides[current].style.display = "block";
+  console.log(current);
+}
+
+prev.onclick = () => {
+  if (current > 0) current -= 1;
+  else current = slides.length - 1;
+  slideShow(current);
 };
+
+next.onclick = () => {
+  if (current < slides.length - 1) current += 1;
+  else current = 0;
+  slideShow(current);
+};
+
+// prev.addEventListener("onclick", () => {
+//   if (current > 0) current -= 1;
+//   else current = slides.length - 1;
+//   console.log(current);
+// });
