c5887bbc0c7c05d62c91a193e86ad9fe4e7c1fd2
mxnxa <alsgk0428@naver.com>
2022-06-27 09:45:40 +0900

    수정

--- slider2/slider.js
+++ slider2/slider.js
@@ -1,19 +1,18 @@
 const slides = document.querySelector(".slides");
 const prev = document.querySelector(".prev");
 const next = document.querySelector(".next");
-const slideImg = document.querySelectorAll(".slides li");
-const count = slideImg.length;
+const count = document.querySelectorAll(".slides li").length;
 let currentIdx = 0;
 
 function moveSlide(num) {
-  slides.style.transform = `translateX(${800 * -num}px)`;
+  slides.style.transform = `translate(${-800 * num}px)`;
   currentIdx = num;
 }
 
-prev.addEventListener("click", function () {
+prev.addEventListener("click", () => {
   if (currentIdx !== 0) moveSlide(currentIdx - 1);
 });
 
-next.addEventListener("click", function () {
-  if (currentIdx !== count - 1) moveSlide(currentIdx + 1);
+next.addEventListener("click", () => {
+  if (currentIdx < count - 1) moveSlide(currentIdx + 1);
 });
